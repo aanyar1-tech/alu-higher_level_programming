c425dbb5e4325f9c08d1bbf8702d4588dc0cca0a
aanyar1-tech <your-email@example.com>
2026-07-14 12:28:05 +0200

    Add MagicClass from bytecode

---
+++ python-classes/103-magic_class.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Module that defines a MagicClass."""
+import math
+
+
+class MagicClass:
+    """Represents a magic class with a radius."""
+
+    def __init__(self, radius=0):
+        """Initialize a new MagicClass.
+
+        Args:
+            radius: the radius.
+
+        Raises:
+            TypeError: if radius is not a number.
+        """
+        self.__radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError("radius must be a number")
+        self.__radius = radius
+
+    def area(self):
+        """Return the area of the circle."""
+        return self.__radius ** 2 * math.pi
+
+    def circumference(self):
+        """Return the circumference of the circle."""
+        return 2 * math.pi * self.__radius
