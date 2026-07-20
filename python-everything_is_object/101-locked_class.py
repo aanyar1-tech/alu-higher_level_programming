eb16de6a41f81192e23ca24c9e137f03c7c4ce2b
aanyar1-tech <a.anyar1@alustudent.com>
2026-07-20 11:40:48 +0200

    Add docstrings to LockedClass

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+"""Module that defines a LockedClass."""
+
+
 class LockedClass:
+    """Represents a class that only allows a first_name attribute."""
     __slots__ = ["first_name"]
