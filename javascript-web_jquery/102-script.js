590cf94bfb08d871b5b1fee5d2b9e2301ab09a5e
aanyar1-tech <a.anyar1@alustudent.com>
2026-09-24 23:23:30 -0700

    Add advanced tasks 100 through 103

---
+++ javascript-web_jquery/102-script.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {$('#btn_translate').click(function () {
+    const lang = $('#language_code').val();
+    $.get(`https://www.fourtonfish.com/hellosalut/hello/?lang=${lang}`, function (data) {
+      $('#hello').text(data.hello);
+    });
+  });
+});
