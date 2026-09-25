3adfedd970502f4273c2f0cf856c3dff0a349f24
aanyar1-tech <a.anyar1@alustudent.com>
2026-09-24 22:36:21 -0700

    Add empty Rectangle class

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -3,14 +3,27 @@
 
 
 def write_file(filename="", text=""):
+<<<<<<< HEAD
     """Write a string to a text file (UTF8), creating or overwriting it.
 
     Args:
         filename (str): the path of the file to write to.
         text (str): the string to write.
+=======
+    """Write a string to a text file (UTF8) and return the number
+    of characters written.
+
+    Args:
+        filename (str): the path of the file to write to.
+        text (str): the string to write to the file.
+>>>>>>> Add write_file function
 
     Returns:
         int: the number of characters written.
     """
+<<<<<<< HEAD
     with open(filename, mode="w", encoding="utf-8") as f:
+=======
+    with open(filename, "w", encoding="utf-8") as f:
+>>>>>>> Add write_file function
         return f.write(text)
